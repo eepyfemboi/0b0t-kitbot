2d7d785e51390ca9d7743d7fceff6bcf02c8b321
CoC-Fire <109904491+CoC-Fire@users.noreply.github.com>
2023-07-28 13:55:24 -0700

    Update KitBot.java
    update version

--- KitBot.java
+++ KitBot.java
@@ -30,7 +30,7 @@ import java.lang.reflect.Field;
 public class KitBot {
     public static final String MODID = "kitbot";
     public static final String NAME = "0b0t KitBot";
-    public static final String VERSION = "1.0.3";
+    public static final String VERSION = "1.0.5";
     private boolean waitingForRespawn = false;
     private boolean scheduledMoveForward = false;
     private boolean waitingForKill = false;
